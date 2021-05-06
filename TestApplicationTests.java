f9cbec66e821793f71961c4d58551aff24dad751
MayuriP111 <83753653+MayuriP111@users.noreply.github.com>
2021-05-06 14:24:49 +0530

    Add files via upload

---
+++ TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.example;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
